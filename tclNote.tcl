ca6fc7eb474b79e3b63f6349aa459d9e1494c49d
Yuji Sode <yuji.sode@gmail.com>
2017-08-21 17:21:38 +0900

    Update tclNote.tcl

--- tclNote.tcl
+++ tclNote.tcl
@@ -7,7 +7,8 @@
 #	See LICENSE or http://opensource.org/licenses/mit-license.php
 ##===================================================================
 #Unicode codepoints sequence friendly text editor.
-#This program is compliant with the Unicode® Standard.
+#This program is compliant with the Unicode Standard.
+#
 #Unicode is a registered trademark of Unicode, Inc. in the United States and other countries.
 #Reference: Unicode, Inc. 1991-2017. the Unicode Consortium. derived on [2017-08-19] and from: http://www.unicode.org/
 #=== global values ===
